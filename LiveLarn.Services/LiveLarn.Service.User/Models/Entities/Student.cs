166038cf27170a81d3d8400dcaf2deee793648ac
FURKAN CAKIR <furkan2834@gmail.com>
2019-07-07 16:07:16 +0300

    user service

---
+++ LiveLarn.Services/LiveLarn.Service.User/Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveLarn.Service.User.Models.Entities
+{
+    public class Student : UserBase
+    {
+        public Enum EducationLevel { get; set; }
+
+        public Enum Scope { get; set; } // = 1 : unspecified
+    }
+}
